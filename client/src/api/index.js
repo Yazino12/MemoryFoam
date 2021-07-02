600e9a6cc3583dca525dcc1d684bbbaf5092c50e
Yazino12 <43172164+Yazino12@users.noreply.github.com>
2021-07-02 19:54:47 +0600

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memory-foam.herokuapp.com/posts";
+const url = "http://localhost:5000";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
